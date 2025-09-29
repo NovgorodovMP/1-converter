3cf9c25e6570de5cd2d18d31c4d42e6eebea01ca
NovgorodovMP <novgorodov.mp@gmail.com>
2025-09-30 00:35:15 +0300

    add input function and empty function for convert

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main 
 
+import (
+	"fmt"
+)
+
 const USD_TO_EUR = 0.83
 const USD_TO_RUB = 83
 const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
 func main() {
+	readUserInput()
+
+}
+
+func readUserInput() string {
+	var userInput string
+	fmt.Print("Enter number: ")
+	fmt.Scanln(&userInput)
+	
+	return userInput
+}
+
+func convertMoney(number float32, currencyFrom, currencyTo string) {
 
 }
